f52da20d3fd9d86dea2cadbd1bf1b924475b820a
juancarlosfield <juancarlosfield@gmail.com>
2016-09-01 18:51:31 -0700

    Create a world hash with keys. each key are full of values and they can be nested, added, depending on the string

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,18 @@
+# Nested Data Structures
+world ={
+   "fantasy" => ['fairies', 'vampires', 'werewolves', 'angels', 'demons'], 
+   "types_of_buildings" => ['highways', 'houses', 'ports'], 
+   "elements" => ['earth', 'fire', 'wind', 'water'], 
+   "types_of_ppl" => ['good', 'bad', 'ugly'], 
+   "landscapes" => ['mountains', 'valleys', 'roads', 'oceans', 'rivers']
+ }
+# Find index for wind
+ #p world ['elements'][2]
+
+# Add the love element 
+# p world ['elements'].push('love')
+
+# p world ['types_of_ppl']
+p world
+
+# between each key and value the next key comes with a come 
