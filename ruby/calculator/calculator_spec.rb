576139a73d2fb0268c8a4a7b56b25a5b5bf5f5de
juancarlosfield <juancarlosfield@gmail.com>
2016-09-10 12:54:36 -0700

    Add divide rspec

--- ruby/calculator/calculator_spec.rb
+++ ruby/calculator/calculator_spec.rb
@@ -23,5 +23,9 @@ describe Calculator do
 
   it "multiplies two integers" do
     expect(calculator.multiply(2,3)).to eq 6
+
+  it "divides two integers" do
+    expect(calculator.divide(8,4)).to eq 2
+
   end
 end
