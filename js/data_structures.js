4155203eaedafb649862b305bbfa420cf35a01ed
juancarlosfield <juancarlosfield@gmail.com>
2016-09-16 12:25:52 -0700

    Add js folder with changes on name and colors

---
+++ js/data_structures.js
@@ -0,0 +1,13 @@
+colors = [
+"blue", "orange", "black", "gray"
+];
+
+names = [
+"Derrick", "JC", "Bill", "James"
+];
+
+colors.push("green");
+console.log(colors);
+
+names.push("Axel");
+console.log(names);
