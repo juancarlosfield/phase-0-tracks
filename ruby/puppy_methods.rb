e5d0523eb62ac0706e8cb71717b5c5f00a257f29
juancarlosfield <juancarlosfield@gmail.com>
2016-10-20 12:38:18 -0700

    Update puppy_methods.rb
    Modified previous file

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,77 +4,129 @@
 # Create methods to make puppies do several commands.
 # fetch, bark a number of times, speak a number of times, my own command (play dead), and initializing new Puppy instances.
 
-# class Puppy
-# end
 
-duchess = Puppy.new
-fido = Puppy.new
-spot = Puppy.new
-gilbert = Puppy.new 
 
-class Puppy
+############
 
+class Puppy 
+  def initialize
+    puts"initializing new puppy instance ..."
+  end
+  
   def fetch(toy)
-    puts "I brought back the #{toy}!"
+    puts "I brought back the #{toy}"
   end
 
-  def bark(times)
-    puts "BARK!" * times.to_i
-  end
-
-  def speak(times)
-    puts "Woof! " * times
+  def speak(int)
+    int = "woof"
   end
 
   def roll_over
-    puts "**rolls over**"
+    puts "roll over!"
   end
 
   def dog_years(human_years)
-    puts  "#{human_years.to_i} human years is equal to #{human_years.to_i * 7} dog years"
+    dog_years = human_years / 7 
+    dog_years 
   end
 
-  def play_dead
-    puts "**plays dead**"
-  end   
-
-    def initialize
-      puts "Initializing new puppy instance ..."
-    end
-
+  def play_dead(dead)
+    dead = "Look im a #{dead} puppy!"
+  end
 end
 
+dog = Puppy.new
+p dog.fetch("thomas")
+p dog.speak(9)
+dog.roll_over
+p dog.dog_years(49)
+p dog.play_dead("dead")
 
+####################  release # 2 ############
 
+class States
+  def initialize(state)
+    @state = state 
+  end
 
-# Release 2
+  def coast
+    puts "#{@state} : information not available" 
+  end
 
-# Make a loop to make 50 instances of whatever class.  
-# The loop has to store all the instances in a data structure [array] 
-# Iterate over that data structure using .each 
-# call the instance methods you wrote on each instance. 
-
-
-
-
-class Flower 
-
-  def initialize
-    puts "Initializing new flower instance..."
-  end 
-        
-  def pluck
-    puts "ouch!"
+  def time_zone
+    puts "#{@state} : time zone not available"
   end
 
 end
 
-flower_pot = []
-50.times do
-flower_pot << Flower.new
-  puts "Yay!"
+list_of_states = [
+"Alabama",
+"Alaska",
+"Arizona",
+"Arkansas", 
+"California",
+"Colorado",
+"Connecticut",
+"Delaware",
+"Florida",
+"Georgia",
+"Hawaii",
+"Idaho", 
+"Illinois",
+"Indiana", 
+"Iowa",
+"Kansas", 
+"Kentucky ",
+"Louisiana", 
+"Maine",
+"Maryland",
+"Massachusetts",
+"Michigan",
+"Minnesota ",
+"Mississippi",
+"Missouri",
+"Montana Nebraska",
+"Nevada",
+"New Hampshire",
+"New Jersey",
+"New Mexico",
+"New York",
+"North Carolina",
+"North Dakota",
+"Ohio",
+"Oklahoma",
+"Oregon" ,
+"Pennsylvania",
+"Rhode Island",
+"South Carolina",
+"South Dakota", 
+"Tennessee",
+"Texas", 
+"Utah", 
+"Vermont",
+"Virginia",
+"Washington", 
+"West Virginia", 
+"Wisconsin", 
+"Wyoming"
+]
+
+US_states = []
+
+list_of_states.each do |a|
+ US_states << States.new(a) # now it has methods
+end
+ # puts "#{US_states}"
+ US_states.each do |b| 
+ b.coast # run into the method 
+ b.time_zone
 end
 
-flower_pot.each do |flower|
-  flower.pluck 
-end
+ # puts US_states
+
+
+
+
+
+
+
